cec4cf4c6f01a9aa6c08ba0c9da43e62e9566987
setsuotokohorikawa <ralralph@gmail.com>
2019-10-01 18:21:39 +0900

    deploy準備

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 server '54.64.249.231', user: 'app', roles: %w{app db web}
-set :ssh_options, keys;: '/Users/setsuotokohorikawa/.ssh/id_rsa'
+set :ssh_options, keys: '/Users/setsuotokohorikawa/.ssh/id_rsa'
